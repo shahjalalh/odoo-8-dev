64ada0eccda7e32a48be172605165b31e6fac73a
Shahjalal Hossain <shahjalal.hossain@zerogravity.com.bd>
2018-03-29 09:03:40 +0600

    odoo-8-dev

---
+++ todo_app/__openerp__.py
@@ -0,0 +1,8 @@
+{
+    'name': 'To-Do Application',
+    'description': 'Manage your personal Tasks with this module',
+    'author': 'Shahjalal',
+    'depends': ['mail'],
+    'data': ['todo_view.xml'],
+    'application': True
+}
