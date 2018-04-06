63e28a87cd2197477e46733cd671b7d1d3a9c7c0
Shahjalal Hossain <shahjalal.tipu@gmail.com>
2018-04-06 12:30:26 +0600

    74

---
+++ todo_user/todo_task.py
@@ -0,0 +1,27 @@
+from openerp import models, fields, api
+
+
+class TodoTask(models.Model):
+    _name = 'todo.task'
+    _inherit = ['todo.task', 'mail.thread']
+
+    name = fields.Char(help="What needs to be done?")
+
+    user_id = fields.Many2one('res.users', 'Responsible')
+    date_deadline = fields.Date('Deadline')
+
+    @api.one
+    def do_toggle_done(self):
+        # import pdb;pdb.set_trace()
+        if self.user_id != self.env.user:
+            raise Exception('Only the responsible can do this!')
+        else:
+            return super(TodoTask, self).do_toggle_done()
+
+    @api.multi
+    def do_clear_done(self):
+        domain = [('is_done','=',True), '|', ('user_id', '=', self.env.uid), ('user_id', '=', False)]
+        done_recs = self.search(domain)
+        done_recs.write({'active': False})
+
+        return True
