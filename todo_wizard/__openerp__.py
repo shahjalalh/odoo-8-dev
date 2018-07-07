ac8d9c55a2210ba3d80bb4f56dc881a9f09fb497
Shahjalal Hossain <shahjalal.hossain@zerogravity.com.bd>
2018-07-07 14:42:17 +0600

    openerp

--- todo_wizard/__openerp__.py
+++ todo_wizard/__openerp__.py
@@ -1,7 +1,9 @@
 {
-    'name': '',
-    'description': '',
-    'author': '',
-    'depends': [''],
-    'data': []
+    'name': 'To-do task Management Assistant',
+    'description': 'Mass edit your to-do backlog',
+    'author': 'Shahjalal',
+    'depends': ['todo_user'],
+    'data': [
+        'todo_wizard_view.xml'
+    ]
 }
